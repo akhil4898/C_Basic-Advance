bd0fa29d1e6ec01783291e99992b39c709807032
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2022-08-30 12:36:08 +0530

    Modified macros file

--- mac.c
+++ mac.c
@@ -1,18 +1,18 @@
 //1. Preprocessors also support if-else directives which are typically used for conditional compilation. 
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// #define COUNTRY "India"                                         // COUNTRY macro...
+#define COUNTRY "India"                                         // COUNTRY macro...
 
-// void main()
-// {
-//     #ifdef COUNTRY                                              // if macro is defined then execute...
-//         printf("%s is the great country...\n", COUNTRY);
-//     #endif
-//     #ifndef COUNTRY                                             // if macro is not defined then execute...
-//         printf("I love  my country...\n");
-//     #endif
-// }
+void main()
+{
+    #ifdef COUNTRY                                              // if macro is defined then execute...
+        printf("%s is the great country...\n", COUNTRY);
+    #endif
+    #ifndef COUNTRY                                             // if macro is not defined then execute...
+        printf("I love  my country...\n");
+    #endif
+}
 
 
 
