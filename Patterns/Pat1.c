02fc669a68cf013df26c430844edcb28add2e6ba
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2022-09-14 15:05:38 +0530

    Add some programs

--- Patterns/Pat1.c
+++ Patterns/Pat1.c
@@ -521,12 +521,56 @@ void pat20(int n)
     
 }
 
+
+void pat21(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            printf("  ");
+        }
+        for (int k = 1; k <= n; k++)
+        {
+            if (i == 1 || i == n || k == 1 || k == n)
+                printf("* ");
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+    
+}
+
+
+void pat22(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j < i; j++)
+        {
+            printf("  ");
+        }
+        for (int k = 1; k <= n; k++)
+        {
+            if (i == 1 || i == n || k == 1 || k == n)
+                printf("* ");
+            else
+                printf("  ");
+        }
+        printf("\n");
+        
+    }
+    
+}
+
+
 int main()
 {
     int rows, cols;
     printf("Enter Rows and Cols: ");
     scanf("%d", &rows);
     printf("\n");
-    pat20(rows);
+    pat22(rows);
     return 0;
 }
