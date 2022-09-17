c437187d3867879332636a24bf437640a142718d
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2022-09-17 23:30:48 +0530

    Add some programs

--- Patterns/Pat1.c
+++ Patterns/Pat1.c
@@ -1027,13 +1027,13 @@ void pat35(int n)
 }
 
 
-void pat35(int n)
+void pat36(int n)
 {
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
         {
-            if (i == 1 || i ==n || j == 1 || j == n || i == j || j == n - i + 1)
+            if (i == j || j == n - i + 1)
                 printf("* ");
             else
                 printf("  ");            
