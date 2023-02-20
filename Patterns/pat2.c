effcae41d7c2cd974398b1cc0cdc310a18cef456
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2023-02-20 11:54:06 +0530

    Update file

--- Patterns/pat2.c
+++ Patterns/pat2.c
@@ -1,3 +1,38 @@
 #include <stdio.h>
 
-void pa
+/*
+
+###
+###
+###
+
+*/
+
+void pattern(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n ; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        
+    }
+}
+
+
+
+
+
+
+
+int main()
+{
+    int num;
+    printf("Enter rows: ");
+    scanf("%d", &num);
+    printf("\n");
+    pattern(num);
+    return 0;
+}
